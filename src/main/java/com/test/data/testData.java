bc5cca21ba5823d754136d7364757c7a78766b42
wangyajing <nancy@zhyea.com>
2022-01-19 17:02:10 +0800

    修改目录

--- src/main/java/com/test/data/testData.java
+++ src/main/java/com/test/data/testData.java
@@ -10,7 +10,7 @@ import com.test.data.getDataUitlClass;
 public class testData {
     @DataProvider(name = "TingcheData")
     public static Object[][] getTestData() throws IOException, InvalidFormatException {
-        String excelPath    = "E:\\testData.xlsx";
+        String excelPath    = "E:\\MyJava\\workspace\\mytest\\src\\main\\java\\com\\test\\data\\testData.xlsx";
         String excelSheet   = "test1";
         return getDataUitlClass.getDataUtil(excelPath,excelSheet);
 
